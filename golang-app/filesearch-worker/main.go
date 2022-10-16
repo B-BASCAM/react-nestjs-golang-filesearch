f904cbe2f0b1081529957c9031ba06a13125882f
ictinaetus_malaiensis <bulentbascam16@gmail.com>
2022-10-16 14:14:03 +0300

    add: logger to filesearch-worker

--- golang-app/filesearch-worker/main.go
+++ golang-app/filesearch-worker/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"golangapp/golang-app/pkg"
+	"golangapp/golang-app/pkg/logger"
+)
+
+func Initialize() {
+	logger.CreateLogger("fw: ", "fileworker.log")
+}
+
 func main() {
 
+	pkg.SetWorkingDirectory()
+
+	go Initialize()
+
+	pkg.WaitForTerminate()
 }
