b83e49c2d013f13062545d5721bfaa488e3543aa
CaganPamir <bulentbascam16@gmail.com>
2022-11-28 13:44:16 +0300

    add: TaskDetail Component/Epic/..

--- react-app/src/service/todosService.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { IAddTodoAction } from '../store/actions/todosActions';
-
-export const addtodoservice = (action: IAddTodoAction): Promise<any> => {
-    return (
-        axios.post("http://localhost:5000/todos", action.payload)
-    )
-}
-
--- react-app/src/types/todoitem.ts
+++
@@ -1,4 +0,0 @@
-export interface ITodoItem {
-  id: number;
-  description: string;
-}
